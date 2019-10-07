5e24793352b073b24b307cfadcd4ff966a5c7b22
Schweizeren <j_wannerhenriksen@hotmail.com>
2019-10-07 12:06:01 +0200

    WeedShopContext

---
+++ WeedShop/WeedShop.InfraStructure.SQL/WeedShopContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeedShop.InfraStructure.SQL
+{
+    public class WeedShopContext : DbContext
+    {
+        public WeedShopContext(DbContextOptions opt) : base(opt)
+        {
+
+        }
+
+        
+
+    }
+}
