29b041c7c452aa88ca45c039136ce3f90f0bd1e0
Frederik Jensen <kungfu15447@users.noreply.github.com>
2019-10-07 13:46:41 +0200

    Tilføjet hurtig mock data

--- WeedShop/WeedShop.RestAPI/Controllers/WeedsController.cs
+++ WeedShop/WeedShop.RestAPI/Controllers/WeedsController.cs
@@ -20,14 +20,28 @@ namespace WeedShop.RestAPI.Controllers
         [HttpGet]
         public ActionResult<List<Weed>> Get([FromQuery] Filter filter)
         {
-            return Ok(_weedServ.GetWeeds(filter));
+            List<Weed> weeds = new List<Weed>()
+            {
+                new Weed{Id = 1, Name = "Weed"},
+                new Weed{Id = 2, Name = "Cannabios"},
+                new Weed{Id = 3, Name = "Khuez"},
+                new Weed{Id = 4, Name = "Krezi mod"},
+                new Weed{Id = 5, Name = "Forhni Feks"},
+                new Weed{Id = 6, Name = "Laco"},
+                new Weed{Id = 7, Name = "Hvad hedder weed?"},
+                new Weed{Id = 8, Name = "KundeMedKre"},
+                new Weed{Id = 9, Name = "Ku'"}
+            };
+            return Ok(weeds);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public ActionResult<Weed> Get(int id)
         {
-            return Ok(_weedServ.GetWeed(id));
+            return Ok(_weedServ.GetWeed(id))
+                
+                ;
         }
 
         // POST api/values
@@ -51,7 +65,7 @@ namespace WeedShop.RestAPI.Controllers
             Weed weed = _weedServ.GetWeed(id);
             if (weed == null)
             {
-                return BadRequest("Could not find specific weed");
+                return BadRequest("Could not find the specific weed to delete");
             }else
             {
                 return Ok(_weedServ.DeleteWeed(weed));
