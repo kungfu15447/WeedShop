f5f70087bc3dc906f68b3fa3b2dc545cea45fdf1
Frederik Jensen <kungfu15447@users.noreply.github.com>
2019-10-07 11:01:32 +0200

    Implementeret interfacet IWeedRepository

--- WeedShop/WeedShop.Core/DomainService/IWeedRepository.cs
+++ WeedShop/WeedShop.Core/DomainService/IWeedRepository.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using WeedShop.Core.Entity;
 
 namespace WeedShop.Core.DomainService
 {
     public interface IWeedRepository
     {
+        Weed ReadWeed(int id);
+        IEnumerable<Weed> ReadWeeds();
+        Weed AddWeed(Weed weed);
+        Weed DeleteWeed(Weed weed);
+        Weed UpdateWeed(Weed weed);
+
 
     }
 }
--- WeedShop/WeedShop.InfraStructure.SQL/Repositories/WeedRepository.cs
+++ WeedShop/WeedShop.InfraStructure.SQL/Repositories/WeedRepository.cs
@@ -1,10 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using WeedShop.Core.DomainService;
+using WeedShop.Core.Entity;
 
 namespace WeedShop.InfraStructure.SQL.Repositories
 {
-    class WeedRepository
+    class WeedRepository : IWeedRepository
     {
+        public Weed AddWeed(Weed weed)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Weed DeleteWeed(Weed weed)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Weed ReadWeed(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Weed> ReadWeeds()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Weed UpdateWeed(Weed weed)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
