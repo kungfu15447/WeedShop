f68b5f511569a737953ff12d182169dd01f41efa
Schweizeren <j_wannerhenriksen@hotmail.com>
2019-10-07 12:22:51 +0200

    WeedShopContext update

--- WeedShop/WeedShop.InfraStructure.SQL/WeedShopContext.cs
+++ WeedShop/WeedShop.InfraStructure.SQL/WeedShopContext.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using WeedShop.Core.Entity;
+using Type = WeedShop.Core.Entity.Type;
 
 namespace WeedShop.InfraStructure.SQL
 {
@@ -12,7 +14,10 @@ namespace WeedShop.InfraStructure.SQL
 
         }
 
-        
+
+        public DbSet<Weed> Weeds { get; set; }
+        public DbSet<Type> Types { get; set; }
+
 
     }
 }
