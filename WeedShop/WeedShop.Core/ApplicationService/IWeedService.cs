ed1d63a209dc19caf259ce9e907197d187fe103e
Schweizeren <j_wannerhenriksen@hotmail.com>
2019-10-07 11:01:16 +0200

    WeedService

--- WeedShop/WeedShop.Core/ApplicationService/IWeedService.cs
+++ WeedShop/WeedShop.Core/ApplicationService/IWeedService.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using WeedShop.Core.Entity;
 
 namespace WeedShop.Core.DomainService
 {
     public interface IWeedService
     {
+        List<Weed> GetWeeds();
+        Weed CreateWeed(Weed weed);
+        Weed UpdateWeed(Weed weed);
+        Weed DeleteWeed(Weed weed);
+        Weed GetWeed(Weed weed);
+
+        
+
+
     }
 }
